5dfc7aae8037f1bcfbfad6a6a6dbb34683b40cf7
gledos <cngledos@gmail.com>
2022-04-06 00:22:13 +0800

    继续测试

--- README.md
+++ README.md
@@ -1,7 +1,43 @@
-# The Bilibili Pixel Drawing Atlas
-
-这里是哔哩哔哩夏日绘板活动的地图集，该项目的代码由 [Roland Rytz](https://github.com/RolandR/place-atlas) 创建，并在 GNU Affero 通用公共许可证 v3.0 下获得许可。
+# The Place Atlas
+The /r/place Atlas is a project aiming to catalog all the artworks created during Reddit's 2022 /r/place event.
+This project was created by Roland Rytz and is licensed under the GNU Affero General Public License v3.0.
 
 ---
 
-你可以点击 [此处](https://delicate-hamster-ab2a05.netlify.app/web/) 访问网站。
+*You can check out the website by clicking [here](https://place-atlas.stefanocoding.me/).*
+
+If you don't know GitHub and wanted to submit new entries or request changes to existing ones, please visit [/r/placeAtlas2](https://www.reddit.com/r/placeAtlas2/).
+
+## How To Contribute
+
+### Map Contributions
+
+<h4><b>WE ONLY ACCEPT NEW CONTRIBUTIONS ON REDDIT</b></h4>
+
+To contribute to the map, we require a certain format for artwork region and labels. This can be generated on the [contributing page](https://place-atlas.stefanocoding.me/index.html?mode=draw) on the website. 
+
+#### Reddit Submission
+
+1. Follow the instructions on the [contributing page](https://place-atlas.stefanocoding.me/index.html?mode=draw), then click "Post Direct to Reddit".
+2. Flair your post with the "New Entry" tag.
+3. A moderator will accept your contribution shortly.
+
+<!--
+
+#### GitHub Submission
+
+1. Create a fork of our repo.
+2. Enter your data into the `web/_js/atlas.js` file, with the correct format and ID number.
+3. Create a Pull Request.
+
+-->
+
+### Map Edits
+
+1. Create a fork of our repo.
+2. Enter your data into the `web/_js/atlas.json` file, with the correct format and ID number.
+3. Create a Pull Request against the `/cleanup` branch.
+
+### Cleaning Contributions
+
+If you spot a duplicate, please PR against `/cleanup`. To help find duplicates, run the code locally, changing line 92 of `main.js` to `overlap`.
--- tools/redditcrawl.py
+++ tools/redditcrawl.py
@@ -1,32 +1,75 @@
 
 import praw
+import json
+import time
 
-outfile = open('temp.js', 'w')
+outfile = open('temp_atlas.json', 'w', encoding='utf-8')
+failfile = open('manual_atlas.json', 'w', encoding='utf-8')
 
 credentials = open('credentials', 'r')
 client_id = credentials.readline().strip(' \t\n\r')
 client_secret = credentials.readline().strip(' \t\n\r')
 
-startId = 1778
-
 reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent='atlas_bot')
 
-for submission in reddit.subreddit('placeAtlas').new(limit=220):
+failcount = 0
+successcount = 0
+
+jsonfile = open("../web/atlas.json", "r")
+existing = json.load(jsonfile)
+
+existing_ids = []
+
+for item in existing:
+	existing_ids.append(item['id'])
+
+
+for submission in reddit.subreddit('placeAtlas2').new(limit=1000):
+	"""
+	Auth setup
+	1. Head to https://www.reddit.com/prefs/apps
+	2. Click "create another app"
+	3. Give it a name and description
+	4. Select "script"
+	5. Redirect to http://localhost:8080
+	6. Copy ID (under Personal Use Script)
+	7. Append to file called "credentials"
+	8. Copy Secret 
+	9. Append on newline to "credentials" file
+	10. Run Script
+
+	Running Script
+	1. Input the next ID to use
+	2. Manually resolve errors in manual_atlas.json
+	3. Copy temp_atlas.json entries into web/_js/atlas.js
+	4. Pull Request
+
+	"""
 	#print(dir(submission))
+	if (submission.id in existing_ids):
+		print("Found first duplicate!")
+		break
 	if(submission.link_flair_text == "New Entry"):
 		text = submission.selftext
-		text = text.replace("\"id\": 0,", "\"id\": 0,\n\t\t\"submitted_by\": \""+submission.author.name+"\",")
+		text = text.replace("\\", "")
+		try:
+			text = text.replace("\"id\": 0,", "\"id\": 0,\n\t\t\"submitted_by\": \""+submission.author.name+"\",")
+		except AttributeError:
+			text = text.replace("\"id\": 0,", "\"id\": 0,\n\t\t\"submitted_by\": \""+"unknown"+"\",")
+
 
 		lines = text.split("\n")
 
 		for i, line in enumerate(lines):
 			if("\"id\": 0" in line):
-				lines[i] = line.replace("\"id\": 0", "\"id\": "+str(startId))
-				startId = startId + 1
-
+				lines[i] = line.replace("\"id\": 0", "\"id\": "+"\""+str(submission.id)+"\"")
 		text = "\n".join(lines)
-		
-		outfile.write(text+",")
-		print("written "+submission.title)
-	else:
-		print("skipped "+submission.title)
+		try:
+			outfile.write(json.dumps(json.loads(text))+",\n")
+		except json.JSONDecodeError:
+			failfile.write(text+",\n")
+			failcount += 1
+		print("written "+submission.id+" submitted "+str(round(time.time()-submission.created_utc))+" seconds ago")
+		successcount += 1
+
+print(f"\n\nSuccess: {successcount}\nFail: {failcount}\nPlease check manual_atlas.txt for failed entries to manually resolve.")
