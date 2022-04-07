792f262cc5314ec62fbb8a5f0eb6459b2d8a964e
gledos <cngledos@gmail.com>
2022-04-07 14:56:41 +0800

    替换 license.txt 的地址

--- 2nd/_js/overlap.js
+++ 2nd/_js/overlap.js
@@ -1,6 +1,3 @@
-
-
-
 /*
 	========================================================================
 	The /r/place Atlas
@@ -18,15 +15,15 @@
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 	For more information, see:
-	http://place-atlas.stefanocoding.me/license.txt
+	https://bilipda.gledos.science/1st/license.txt
 
 	========================================================================
 */
 
-function initOverlap(){
+function initOverlap() {
 
 	var wrapper = document.getElementById("wrapper");
-	
+
 	var objectsContainer = document.getElementById("objectsList");
 
 	var linesCanvas = document.getElementById("linesCanvas");
@@ -46,9 +43,9 @@ function initOverlap(){
 	var entriesLimit = 50;
 	var entriesOffset = 0;
 	var moreEntriesButton = document.createElement("button");
-	moreEntriesButton.innerHTML = "Show "+entriesLimit+" more";
+	moreEntriesButton.innerHTML = "Show " + entriesLimit + " more";
 	moreEntriesButton.id = "moreEntriesButton";
-	moreEntriesButton.onclick = function(){
+	moreEntriesButton.onclick = function () {
 		buildObjectsList();
 	};
 
@@ -72,12 +69,12 @@ function initOverlap(){
 		highlightEntryFromUrl();
 	}*/
 
-	if(document.documentElement.clientWidth > 1280){
+	if (document.documentElement.clientWidth > 1280) {
 		entriesListShown = true;
 		wrapper.className = wrapper.className.replace(/ listHidden/g, "");
 	}
 
-	if(document.documentElement.clientWidth < 1280){
+	if (document.documentElement.clientWidth < 1280) {
 		entriesListShown = false;
 		wrapper.className += " listHidden";
 	}
@@ -86,21 +83,21 @@ function initOverlap(){
 	render();
 	updateLines();
 
-	
+
 
 	var args = window.location.search;
-	if(args){
+	if (args) {
 		id = args.split("id=")[1];
-		if(id){
+		if (id) {
 			highlightEntryFromUrl();
 		}
 	}
 
-	container.addEventListener("mousemove", function(e){
+	container.addEventListener("mousemove", function (e) {
 		updateHovering(e);
 	});
 
-	filterInput.addEventListener("input", function(e){
+	filterInput.addEventListener("input", function (e) {
 		entriesOffset = 0;
 		entriesList.innerHTML = "";
 		entriesList.appendChild(moreEntriesButton);
@@ -109,7 +106,7 @@ function initOverlap(){
 
 	});
 
-	document.getElementById("sort").addEventListener("input", function(e){
+	document.getElementById("sort").addEventListener("input", function (e) {
 		entriesOffset = 0;
 		entriesList.innerHTML = "";
 		entriesList.appendChild(moreEntriesButton);
@@ -117,10 +114,10 @@ function initOverlap(){
 		buildObjectsList(filterInput.value.toLowerCase());
 
 	});
-	
-	hideListButton.addEventListener("click", function(e){
+
+	hideListButton.addEventListener("click", function (e) {
 		entriesListShown = !entriesListShown;
-		if(entriesListShown){
+		if (entriesListShown) {
 			wrapper.className = wrapper.className.replace(/ listHidden/g, "");
 		} else {
 			wrapper.className += " listHidden";
@@ -131,27 +128,27 @@ function initOverlap(){
 		updateLines();
 	});
 
-	function highlightEntryFromUrl(){
+	function highlightEntryFromUrl() {
 
 		var objectsContainer = document.getElementById("objectsList");
 
 		var id = 0;
-		
+
 		var args = window.location.search;
-		if(args){
+		if (args) {
 			id = args.split("id=")[1];
-			if(id){
+			if (id) {
 				id = id.split("&")[0];
 			}
 		}
 
 		//var id = parseInt(window.location.hash.substring(3));
-		
-		var entry = atlas.filter(function(e){
+
+		var entry = atlas.filter(function (e) {
 			return e.id === id;
 		});
 
-		if (entry.length === 1){
+		if (entry.length === 1) {
 			entry = entry[0];
 			var infoElement = createInfoBlock(entry);
 			objectsContainer.innerHTML = "";
@@ -162,14 +159,14 @@ function initOverlap(){
 
 			zoom = 4;
 			applyView();
-			
+
 			zoomOrigin = [
-				 innerContainer.clientWidth/2  - entry.center[0]* zoom// + container.offsetLeft
-				,innerContainer.clientHeight/2 - entry.center[1]* zoom// + container.offsetTop
+				innerContainer.clientWidth / 2 - entry.center[0] * zoom // + container.offsetLeft
+				, innerContainer.clientHeight / 2 - entry.center[1] * zoom // + container.offsetTop
 			];
 
 			//console.log(zoomOrigin);
-			
+
 			applyView();
 			hovered = [entry];
 			render();
@@ -179,26 +176,25 @@ function initOverlap(){
 		}
 	}
 
-	function updateHovering(e){
-		if(!dragging && !fixed){
+	function updateHovering(e) {
+		if (!dragging && !fixed) {
 			var pos = [
-				 (e.clientX - (container.clientWidth/2 - innerContainer.clientWidth/2 + zoomOrigin[0] + container.offsetLeft))/zoom
-				,(e.clientY - (container.clientHeight/2 - innerContainer.clientHeight/2 + zoomOrigin[1] + container.offsetTop))/zoom
+				(e.clientX - (container.clientWidth / 2 - innerContainer.clientWidth / 2 + zoomOrigin[0] + container.offsetLeft)) / zoom, (e.clientY - (container.clientHeight / 2 - innerContainer.clientHeight / 2 + zoomOrigin[1] + container.offsetTop)) / zoom
 			];
 
-			if(pos[0] <= 1280 + 100 && pos[0] >= -100 && pos[0] <= 1280 + 100 && pos[0] >= -100){
+			if (pos[0] <= 1280 + 100 && pos[0] >= -100 && pos[0] <= 1280 + 100 && pos[0] >= -100) {
 				var newHovered = [];
-				for(var i = 0; i < atlas.length; i++){
-					if(pointIsInPolygon(pos, atlas[i].path)){
+				for (var i = 0; i < atlas.length; i++) {
+					if (pointIsInPolygon(pos, atlas[i].path)) {
 						newHovered.push(atlas[i]);
 					}
 				}
 
 				var changed = false;
 
-				if(hovered.length == newHovered.length){
-					for(var i = 0; i < hovered.length; i++){
-						if(hovered[i].id != newHovered[i].id){
+				if (hovered.length == newHovered.length) {
+					for (var i = 0; i < hovered.length; i++) {
+						if (hovered[i].id != newHovered[i].id) {
 							changed = true;
 							break;
 						}
@@ -207,12 +203,12 @@ function initOverlap(){
 					changed = true;
 				}
 
-				if(changed){
+				if (changed) {
 					hovered = newHovered;
 
 					objectsContainer.innerHTML = "";
 
-					for(var i in hovered){
+					for (var i in hovered) {
 						var element = createInfoBlock(hovered[i]);
 
 						objectsContainer.appendChild(element);
@@ -227,24 +223,24 @@ function initOverlap(){
 		}
 	}
 
-	function renderBackground(){
+	function renderBackground() {
 
 		backgroundContext.clearRect(0, 0, canvas.width, canvas.height);
 
 		backgroundContext.fillStyle = "rgba(255, 255, 255, 1)";
 		backgroundContext.fillRect(0, 0, canvas.width, canvas.height);
 
-		for(var i = 0; i < atlas.length; i++){
+		for (var i = 0; i < atlas.length; i++) {
 
 			var path = atlas[i].path;
 
 			backgroundContext.beginPath();
 
-			if(path[0]){
+			if (path[0]) {
 				backgroundContext.moveTo(path[0][0], path[0][1]);
 			}
 
-			for(var p = 1; p < path.length; p++){
+			for (var p = 1; p < path.length; p++) {
 				backgroundContext.lineTo(path[p][0], path[p][1]);
 			}
 
@@ -257,31 +253,31 @@ function initOverlap(){
 		var pixels = backgroundContext.getImageData(0, 0, backgroundCanvas.width, backgroundCanvas.height).data;
 		var blank = 0;
 
-		for(var i = 0; i < pixels.length; i+=4){
-			if(pixels[i] == 255){
+		for (var i = 0; i < pixels.length; i += 4) {
+			if (pixels[i] == 255) {
 				blank++;
 			}
 		}
 
-		console.log(blank+" blank pixels, which are "+Math.round(blank/100)/100+"% of the canvas ("+(100-Math.round(blank/100)/100)+"% mapped)");
+		console.log(blank + " blank pixels, which are " + Math.round(blank / 100) / 100 + "% of the canvas (" + (100 - Math.round(blank / 100) / 100) + "% mapped)");
 	}
 
-	function buildObjectsList(filter){
+	function buildObjectsList(filter) {
 
-		if(entriesList.contains(moreEntriesButton)){
+		if (entriesList.contains(moreEntriesButton)) {
 			entriesList.removeChild(moreEntriesButton);
 		}
 
 		var sortedAtlas;
 
-		if(filter){
-			sortedAtlas = atlas.filter(function(value){
+		if (filter) {
+			sortedAtlas = atlas.filter(function (value) {
 				return (value.name.toLowerCase().indexOf(filter) !== -1);
 			});
-			document.getElementById("atlasSize").innerHTML = "Found "+sortedAtlas.length+" entries.";
+			document.getElementById("atlasSize").innerHTML = "Found " + sortedAtlas.length + " entries.";
 		} else {
 			sortedAtlas = atlas.concat();
-			document.getElementById("atlasSize").innerHTML = "The Atlas contains "+sortedAtlas.length+" entries.";
+			document.getElementById("atlasSize").innerHTML = "The Atlas contains " + sortedAtlas.length + " entries.";
 		}
 
 		var sort = document.getElementById("sort").value;
@@ -290,48 +286,48 @@ function initOverlap(){
 
 		//console.log(sort);
 
-		switch(sort){
+		switch (sort) {
 			case "alphaAsc":
-				sortFunction = function(a, b){
+				sortFunction = function (a, b) {
 					return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
 				}
-			break;
+				break;
 			case "alphaDesc":
-				sortFunction = function(a, b){
+				sortFunction = function (a, b) {
 					return b.name.toLowerCase().localeCompare(a.name.toLowerCase());
 				}
-			break;
+				break;
 			case "newest":
-				sortFunction = function(a, b){
+				sortFunction = function (a, b) {
 					if (a.id > b.id) {
 						return -1;
 					}
 					if (a.id < b.id) {
 						return 1;
 					}
-						// a must be equal to b
+					// a must be equal to b
 					return 0;
 				}
-			break;
+				break;
 			case "oldest":
-				sortFunction = function(a, b){
+				sortFunction = function (a, b) {
 					if (a.id < b.id) {
 						return -1;
 					}
 					if (a.id > b.id) {
 						return 1;
 					}
-						// a must be equal to b
+					// a must be equal to b
 					return 0;
 				}
-			break;
+				break;
 		}
 
 		sortedAtlas.sort(sortFunction);
 
-		for(var i = entriesOffset; i < entriesOffset+entriesLimit; i++){
+		for (var i = entriesOffset; i < entriesOffset + entriesLimit; i++) {
 
-			if(i >= sortedAtlas.length){
+			if (i >= sortedAtlas.length) {
 				break;
 			}
 
@@ -340,17 +336,17 @@ function initOverlap(){
 
 			element.entry = sortedAtlas[i];
 
-			element.addEventListener("mouseenter", function(e){
-				if(!fixed && !dragging){
+			element.addEventListener("mouseenter", function (e) {
+				if (!fixed && !dragging) {
 					objectsContainer.innerHTML = "";
 					zoomOrigin = [
-						 innerContainer.clientWidth/2  - this.entry.center[0]* zoom// + container.offsetLeft
-						,innerContainer.clientHeight/2 - this.entry.center[1]* zoom// + container.offsetTop
+						innerContainer.clientWidth / 2 - this.entry.center[0] * zoom // + container.offsetLeft
+						, innerContainer.clientHeight / 2 - this.entry.center[1] * zoom // + container.offsetTop
 					]
 
 					//console.log(zoomOrigin);
 
-					
+
 					applyView();
 					hovered = [this.entry];
 					render();
@@ -360,8 +356,8 @@ function initOverlap(){
 
 			});
 
-			element.addEventListener("mouseleave", function(e){
-				if(!fixed && !dragging){
+			element.addEventListener("mouseleave", function (e) {
+				if (!fixed && !dragging) {
 					hovered = [];
 					updateLines();
 					render();
@@ -374,35 +370,35 @@ function initOverlap(){
 
 		entriesOffset += entriesLimit;
 
-		if(sortedAtlas.length > entriesOffset){
-			moreEntriesButton.innerHTML = "Show "+Math.min(entriesLimit, sortedAtlas.length - entriesOffset)+" more";
+		if (sortedAtlas.length > entriesOffset) {
+			moreEntriesButton.innerHTML = "Show " + Math.min(entriesLimit, sortedAtlas.length - entriesOffset) + " more";
 			entriesList.appendChild(moreEntriesButton);
 		}
 	}
 
-	function render(){
+	function render() {
 		context.globalCompositeOperation = "source-over";
 		context.clearRect(0, 0, canvas.width, canvas.height);
 
-		if(hovered.length > 0){
+		if (hovered.length > 0) {
 			container.style.cursor = "pointer";
 		} else {
 			container.style.cursor = "default";
 		}
 
 
-		for(var i = 0; i < hovered.length; i++){
+		for (var i = 0; i < hovered.length; i++) {
 
 
 			var path = hovered[i].path;
 
 			context.beginPath();
 
-			if(path[0]){
+			if (path[0]) {
 				context.moveTo(path[0][0], path[0][1]);
 			}
 
-			for(var p = 1; p < path.length; p++){
+			for (var p = 1; p < path.length; p++) {
 				context.lineTo(path[p][0], path[p][1]);
 			}
 
@@ -417,17 +413,17 @@ function initOverlap(){
 		context.globalCompositeOperation = "source-out";
 		context.drawImage(backgroundCanvas, 0, 0);
 
-		for(var i = 0; i < hovered.length; i++){
+		for (var i = 0; i < hovered.length; i++) {
 
 			var path = hovered[i].path;
 
 			context.beginPath();
 
-			if(path[0]){
+			if (path[0]) {
 				context.moveTo(path[0][0], path[0][1]);
 			}
 
-			for(var p = 1; p < path.length; p++){
+			for (var p = 1; p < path.length; p++) {
 				context.lineTo(path[p][0], path[p][1]);
 			}
 
@@ -442,37 +438,35 @@ function initOverlap(){
 
 	}
 
-	function toggleFixed(e){
-		if(!fixed && hovered.length == 0){
+	function toggleFixed(e) {
+		if (!fixed && hovered.length == 0) {
 			return 0;
 		}
 		fixed = !fixed;
-		if(!fixed){
+		if (!fixed) {
 			updateHovering(e);
 			render();
 		}
 	}
 
-	function updateLines(){
+	function updateLines() {
 
 		linesCanvas.width = linesCanvas.clientWidth;
 		linesCanvas.height = linesCanvas.clientHeight;
 		linesContext.lineCap = "round";
-		linesContext.lineWidth = Math.max(Math.min(zoom*1.5, 16*1.5), 6);
+		linesContext.lineWidth = Math.max(Math.min(zoom * 1.5, 16 * 1.5), 6);
 		linesContext.strokeStyle = "#000000";
 
-		for(var i = 0; i < hovered.length; i++){
+		for (var i = 0; i < hovered.length; i++) {
 			var element = hovered[i].element;
 
 			linesContext.beginPath();
 			//linesContext.moveTo(element.offsetLeft + element.clientWidth - 10, element.offsetTop + 20);
 			linesContext.moveTo(
-				 element.getBoundingClientRect().left + document.documentElement.scrollLeft + element.clientWidth/2
-				,element.getBoundingClientRect().top + document.documentElement.scrollTop + 20
+				element.getBoundingClientRect().left + document.documentElement.scrollLeft + element.clientWidth / 2, element.getBoundingClientRect().top + document.documentElement.scrollTop + 20
 			);
 			linesContext.lineTo(
-				 ~~(hovered[i].center[0]*zoom) + innerContainer.offsetLeft
-				,~~(hovered[i].center[1]*zoom) + innerContainer.offsetTop
+				~~(hovered[i].center[0] * zoom) + innerContainer.offsetLeft, ~~(hovered[i].center[1] * zoom) + innerContainer.offsetTop
 			);
 			linesContext.stroke();
 		}
@@ -480,17 +474,15 @@ function initOverlap(){
 		linesContext.lineWidth = Math.max(Math.min(zoom, 16), 4);
 		linesContext.strokeStyle = "#FFFFFF";
 
-		for(var i = 0; i < hovered.length; i++){
+		for (var i = 0; i < hovered.length; i++) {
 			var element = hovered[i].element;
 
 			linesContext.beginPath();
 			linesContext.moveTo(
-				 element.getBoundingClientRect().left + document.documentElement.scrollLeft + element.clientWidth/2
-				,element.getBoundingClientRect().top + document.documentElement.scrollTop + 20
+				element.getBoundingClientRect().left + document.documentElement.scrollLeft + element.clientWidth / 2, element.getBoundingClientRect().top + document.documentElement.scrollTop + 20
 			);
 			linesContext.lineTo(
-				 ~~(hovered[i].center[0]*zoom) + innerContainer.offsetLeft
-				,~~(hovered[i].center[1]*zoom) + innerContainer.offsetTop
+				~~(hovered[i].center[0] * zoom) + innerContainer.offsetLeft, ~~(hovered[i].center[1] * zoom) + innerContainer.offsetTop
 			);
 			linesContext.stroke();
 		}
@@ -501,42 +493,41 @@ function initOverlap(){
 	window.addEventListener("dblClick", updateLines);
 	window.addEventListener("wheel", updateLines);
 
-	container.addEventListener("mousedown", function(e){
+	container.addEventListener("mousedown", function (e) {
 		lastPos = [
-			 e.clientX
-			,e.clientY
+			e.clientX, e.clientY
 		];
 	});
 
-	container.addEventListener("mouseup", function(e){
-		if(Math.abs(lastPos[0] - e.clientX) + Math.abs(lastPos[1] - e.clientY) <= 4){
+	container.addEventListener("mouseup", function (e) {
+		if (Math.abs(lastPos[0] - e.clientX) + Math.abs(lastPos[1] - e.clientY) <= 4) {
 			toggleFixed(e);
 		}
 	});
 
-	objectsContainer.addEventListener("scroll", function(e){
+	objectsContainer.addEventListener("scroll", function (e) {
 		updateLines();
 	});
 
-	window.addEventListener("resize", function(){
+	window.addEventListener("resize", function () {
 		//console.log(document.documentElement.clientWidth, document.documentElement.clientHeight);
 
-		if(document.documentElement.clientWidth > 1280 && viewportWidth <= 1280){
+		if (document.documentElement.clientWidth > 1280 && viewportWidth <= 1280) {
 			entriesListShown = true;
 			wrapper.className = wrapper.className.replace(/ listHidden/g, "");
 		}
 
-		if(document.documentElement.clientWidth < 1280 && viewportWidth >= 1280){
+		if (document.documentElement.clientWidth < 1280 && viewportWidth >= 1280) {
 			entriesListShown = false;
 			wrapper.className += " listHidden";
 		}
 
 		viewportWidth = document.documentElement.clientWidth;
-		
+
 		applyView();
 		render();
 		updateLines();
-		
+
 	});
 
 }
